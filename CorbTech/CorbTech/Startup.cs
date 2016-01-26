5fc216d8c0439ec663540d382363fb46fb9917dd
MattMorden <mattymorden@gmail.com>
2016-01-26 18:13:47 -0500

    Created MVC application

---
+++ CorbTech/CorbTech/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CorbTech.Startup))]
+namespace CorbTech
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
